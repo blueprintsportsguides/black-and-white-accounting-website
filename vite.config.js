a50f5b67aa9f0a8f029583ebe426a325e67f11c3
Samuel Franklin <hello@blueprintsportsguides.com>
2026-01-07 15:48:29 +0000

    version 2

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,32 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        about: './about.html',
+        contact: './contact.html',
+        services: './services.html',
+        'services-tax': './services-tax.html',
+        'services-accounts': './services-accounts.html',
+        'services-advisory': './services-advisory.html',
+        sectors: './sectors.html',
+        'sectors-construction': './sectors-construction.html',
+        'sectors-ecommerce': './sectors-ecommerce.html',
+        'sectors-healthcare': './sectors-healthcare.html',
+        'sectors-hospitality': './sectors-hospitality.html',
+        'sectors-professional-services': './sectors-professional-services.html',
+        'sectors-property': './sectors-property.html',
+        'sectors-retail': './sectors-retail.html',
+        'sectors-startups': './sectors-startups.html',
+        'sectors-trades': './sectors-trades.html',
+        blog: './blog.html',
+        'blog-post': './blog-post.html',
+        privacy: './privacy.html',
+        terms: './terms.html'
+      }
+    }
   }
 });
 
