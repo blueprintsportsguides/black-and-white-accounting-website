8af895f99ee5cc02a8191ac6a00bc7e7e08e7825
Samuel Franklin <hello@blueprintsportsguides.com>
2026-01-21 10:17:10 +0000

    Add MTD landing page for paid advertising - conversion-focused at /mtd

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
       rollupOptions: {
       input: {
         main: './index.html',
+        mtd: './mtd.html',
         about: './about.html',
         contact: './contact.html',
         services: './services.html',
